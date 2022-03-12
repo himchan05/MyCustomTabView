802f565dffa6f4680da8c9cd8ba9180df218c1c9
박힘찬 <himchan05@naver.com>
2022-03-12 22:03:21 +0900

    Initial Commit

---
+++ MyCustomTabView/MyCustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomTabViewApp.swift
+//  MyCustomTabView
+//
+//  Created by 박힘찬 on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct MyCustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
